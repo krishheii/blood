a0b453fd04e61dbdc9948a2c1171375ca43ecb60
Krishnanunny H <31624624+krishheii@users.noreply.github.com>
2018-10-24 21:00:13 +0530

    admin side nav updated

--- admin_side_nav.php
+++ admin_side_nav.php
@@ -22,6 +22,7 @@ else
 	<li><a href="admin_ndonor.php"><i class="fa fa-users fa-lg text-danger"></i> Not Active Donors</a></li>
 	<li><a href="admin_need_blood.php"><i class="fa fa-bed fa-lg"></i> Need Blood</a></li>
 	<li><a href="admin_need_stock.php"><i class="fa fa-shopping-cart fa-lg"></i> Stock Details</a></li>
+	<li><a href="admin_delivery.php"><i class="fa fa-truck fa-lg"></i> Delivery Item</a></li>
 	<hr>
 	<li><a href="#add" data-toggle="collapse" ><i class="fa fa-cogs fa-lg"></i> Settings</a></li>
 	<ul class="nav collapse" id="add">
