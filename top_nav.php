90db94b34dcf852a242805f02c4e1d6bbc71ac4d
Krishnanunny H <31624624+krishheii@users.noreply.github.com>
2018-10-25 11:53:17 +0530

    updated

--- top_nav.php
+++ top_nav.php
@@ -15,7 +15,7 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
-                    <li><a href="about.php"><i class="fa fa-users"></i> Login</a></li>
+                    <li><a href="login.php"><i class="fa fa-users"></i> Login</a></li>
                     <li><a href="Donor_reg.php"><i class="fa fa-briefcase"></i> Services</a></li>
                     <li><a href="contact.php"><i class="fa fa-envelope"></i> Feedback</a></li>
                     <li><a href="admin.php"><i class="fa fa-user-md"></i> Admin</a></li>
