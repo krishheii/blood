079b69c1f87a41296af8c3329bbaa1b5ee929233
Krishnanunny H <31624624+krishheii@users.noreply.github.com>
2018-10-31 13:51:38 +0530

    login final

--- login.php
+++ login.php
@@ -40,7 +40,7 @@
 							            <div id="login-row" class="row justify-content-center align-items-center text-primary">
 							                <div id="login-column" class="col-md-6">
 							                    <div id="login-box" class="col-md-12">
-							                        <form id="login-form" class="form" action="" method="post">
+							                        <form id="login-form" class="form" action="profile_view_donor.php" method="post">
 							                            <h3 class="text-center text-primary ">Login</h3>
 							                            <div class="form-group">
 							                                <label for="username" class="text-primary">Username:</label><br>
@@ -48,14 +48,14 @@
 							                            </div>
 							                            <div class="form-group">
 							                                <label for="password" class="text-primary">Password:</label><br>
-							                                <input type="text" name="password" id="password" class="form-control">
+							                                <input type="password" name="password" id="password" class="form-control">
 							                            </div>
 							                            <div class="form-group">
 
 							                                <input type="submit" name="submit" class="btn btn-info btn-danger" value="submit">
 							                            </div>
 							                            <div id="register-link" class="text-right">
-							                                <a href="#" class="text-primary">Register here</a>
+							                                <a href="Donor_reg.php" class="text-primary">Register here</a>
 							                            </div>
 							                        </form>
 							                    </div>
