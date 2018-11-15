90c079216d453c08c4170414cab22cb9d17d9281
Krishnanunny H <31624624+krishheii@users.noreply.github.com>
2018-11-15 10:37:05 +0530

    request updated

--- request_blood.php
+++ request_blood.php
@@ -6,8 +6,8 @@
 
 </head>
 <body>
-   
-   
+
+
 <?php
 include("top_nav.php");
 ?>
@@ -23,9 +23,9 @@ include("top_nav.php");
 
 
 
-		
 
-       
+
+
 		<div class="row centered-form">
 		    <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
                 <div class="panel panel-primary">
@@ -34,7 +34,7 @@ include("top_nav.php");
                     </div>
                     <div class="panel-body">
 					<p id="errorBox"></p>
-					
+
 					<?php
 						if(isset($_POST["submit"]))
 						{
@@ -44,13 +44,13 @@ include("top_nav.php");
 								{
 									$target_file = $target_dir.rand(100,999). basename($_FILES["PIC"]["name"]);
 									move_uploaded_file($_FILES["PIC"]["tmp_name"], $target_file);
-									
+
 								}
 								else
 								{
-									$target_file ="request_image/no-image.jpg";	
+									$target_file ="request_image/no-image.jpg";
 								}
-								
+
 								 $sql="INSERT INTO request_blood(NAME,GENDER,BLOOD,BUNIT,HOSP,CITY,PIN,DOC,RDATE,CNAME,EMAIL,CON1,CON2,REASON,PIC,CADDRESS)
 								 VALUES('{$_POST["NAME"]}','{$_POST["GENDER"]}','{$_POST["BLOOD"]}','{$_POST["BUNIT"]}','{$_POST["HOSP"]}','{$_POST["CITY"]}','{$_POST["PIN"]}','{$_POST["DOC"]}','{$_POST["RDATE"]}','{$_POST["CNAME"]}','{$_POST["EMAIL"]}','{$_POST["CON1"]}','{$_POST["CON2"]}','{$_POST["REASON"]}','{$target_file}','{$_POST["CADDRESS"]}')";
 									if($con->query($sql))
@@ -62,48 +62,37 @@ include("top_nav.php");
 										echo "<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Error : </strong>Server busy.Try again later.</div>";
 									}
 						}
-						
+
 					?>
 					<form autocomplete="off" method="post"  action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
 						<div class="form-group">
 							<label class="control-label text-primary">Patient Name</label>
 							<input type="text" placeholder="Patient Name" name="NAME"  required id="NAME" class="form-control input-sm">
 						</div>
-						
+
 								<div class="form-group">
 							<label class="control-label text-primary"  for="GENDER">Gender</label>
 								<select id="gen" name="GENDER" required class="form-control input-sm">
 									<option value="">Select Gender</option>
 									<option value="Male">Male</option>
 									<option value="Female">Female</option>
-									<option value="Thirunangai">Thirunangai</option>
-									<option value="Thirunambi">Thirunambi</option>
+									
 								</select>
 						</div>
-						
-						
+
+
 						<div class="form-group">
 							<label class="control-label text-primary">Required Blood Group</label>
 								<select name="BLOOD" id="BLOOD" required  class="form-control input-sm">
 							<option value="">Select Blood</option>
-							<option value="A+">A+</option>
-							<option value="B+">B+</option>
-							<option value="O+">O+</option>
-							<option value="AB+">AB+</option>
-							<option value="A1+">A1+</option>
-							<option value="A2+">A2+</option>
-							<option value="A1B+">A1B+</option>
-							<option value="A2B+">A2B+</option>
-							<option value="A-">A-</option>
-							<option value="B-">B-</option>
-							<option value="O-">O-</option>
-							<option value="AB-">AB-</option>
-							<option value="A1-">A1-</option>
-							<option value="A2-">A2-</option>
-							<option value="A1B">A1B-</option>
-							<option value="A2B">A2B-</option>
-							<option value="A2B">Bombay o+</option>
-							<option value="A2B">Bombay o-</option>
+							<option value="A+">AB-negative</option>
+							<option value="B+">B-negative</option>
+							<option value="O+">AB-positive </option>
+							<option value="AB+">A-negative</option>
+							<option value="A1+">O-negative</option>
+							<option value="A2+">B-positive</option>
+							<option value="A1B+">O-positive</option>
+
 
 								</select>
 						</div>
@@ -131,7 +120,7 @@ include("top_nav.php");
 							<label class="control-label text-primary">When Required</label>
 							<input type="text" placeholder="MM/DD/YYYY" class="form-control input-sm DATES" name="RDATE" id="RDATE">
 						</div>
-						
+
 						<div class="form-group">
 							<label class="control-label text-primary">Contact Name</label>
 							<input type="text" placeholder="Contact Name" class="form-control input-sm" name="CNAME" id="CNAME">
@@ -160,8 +149,8 @@ include("top_nav.php");
 							<label class="control-label text-primary" >Upload Photo</label>
 							<input type="file"  onChange="validate(this.value)" name="PIC" id="PIC">
 						  </div>
-						  
-						  
+
+
 						   <div class="form-group">
 							<button class="btn btn-primary" id="BTN" name="submit"><i class="fa fa-send"></i> Request Now</button>
 						  </div>
@@ -170,7 +159,7 @@ include("top_nav.php");
                 </div>
             </div>
             </div>
-        
+
         </div>
 
 
@@ -178,7 +167,7 @@ include("top_nav.php");
 <script>
 	$(document).ready(
 		function(){
-		
+
 		$("#BTN").click(function(){
 			var NAME=$("#NAME").val();
 			var BLOOD=$("#BLOOD").val();
@@ -200,42 +189,42 @@ include("top_nav.php");
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter Full Name.</div>");
 					return false;
 				}
-		
+
 		if($("#BLOOD").val() == "" )
 				{
 					$("#BLOOD").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Select Blood.</div>");
 					return false;
 				}
-		
+
 				if($("#BUNIT").val() == "")
 				{
 					$("#BUNIT").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter No Of Units.</div>");
 					return false;
 				}
-				
+
 				if(isNaN($("#BUNIT").val()))
 				{
 					$("#BUNIT").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Unit should be numeric.</div>");
 					return false;
 				}
-				
+
 				if($("#HOSP").val() == "")
 				{
 					$("#HOSP").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter Hospital Name and full address.</div>");
 					return false;
 				}
-				
+
 				if($("#CITY").val() == "")
 				{
 					$("#CITY").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter Your city name correctly.</div>");
 					return false;
 				}
-				
+
 				if($("#PIN").val() == "")
 				{
 					$("#PIN").focus();
@@ -254,21 +243,21 @@ include("top_nav.php");
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter Docter Name.</div>");
 					return false;
 				}
-				
+
 				if($("#RDATE").val() == "")
 				{
 					$("#RDATE").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Select the Blood Requiered Date .</div>");
 					return false;
 				}
-				
+
 				if($("#CNAME").val() == "")
 				{
 					$("#CNAME").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter Contact Person Name.</div>");
 					return false;
 				}
-				
+
 				if($("#CADDRESS").val() == "")
 				{
 					$("#CADDRESS").focus();
@@ -281,43 +270,43 @@ include("top_nav.php");
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter your Mobile Number.</div>");
 					return false;
 				}
-				
+
 				if(isNaN($("#CON1").val()))
 				{
 					$("#CON1").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Mobile Number should be Numeric.</div>");
 					return false;
 				}
-				
+
 				if($("#CON2").val() == "")
 				{
 					$("#CON2").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter  Alternate Contact No.</div>");
 					return false;
 				}
-				
+
 				if(isNaN($("#CON2").val()))
 				{
 					$("#CON2").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Alternate Contact should be Numeric.</div>");
 					return false;
 				}
-				
+
 				if($("#REASON").val() == "")
 				{
 					$("#REASON").focus();
 					$("#errorBox").html("<div class='alert alert-danger fade in' ><a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a><strong>Warning : </strong> Please Enter the correct Reason for Blood.</div>");
 					return false;
 				}
-				
-		
-		});	
+
+
+		});
 	});
-	
-	
+
+
 	function validate(file) {
     var ext = file.split(".");
-    ext = ext[ext.length-1].toLowerCase();      
+    ext = ext[ext.length-1].toLowerCase();
     var arrayExtensions = ["jpg" , "jpeg", "png", "bmp", "gif"];
     if (arrayExtensions.lastIndexOf(ext) == -1) {
         alert("Please upload image file only.");
@@ -325,6 +314,6 @@ include("top_nav.php");
     }
 }
 </script>
-	
+
 </body>
 </html>
